6cd1bd87f0cb278a8bd358d39ce3faabc94a0940
kieuhuan <kieuhuanbka@gmail.com>
2022-04-13 09:28:53 +0700

    dev

--- src/main/java/com/codegym/case_study_m4/repository/IUserRepository.java
+++ src/main/java/com/codegym/case_study_m4/repository/IUserRepository.java
@@ -3,6 +3,7 @@ package com.codegym.case_study_m4.repository;
 import com.codegym.case_study_m4.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IUserRepository extends JpaRepository<User, Long>  {
+public interface
+IUserRepository extends JpaRepository<User, Long>  {
     User findByUsername(String username);
 }
