ec52d18906cb9962c393f0e85d5474bd1c7fe02e
kieuhuan <kieuhuanbka@gmail.com>
2022-04-13 15:38:14 +0700

    add sercurity

---
+++ src/main/java/com/codegym/case_study_m4/config/security/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.codegym.case_study_m4.config.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
