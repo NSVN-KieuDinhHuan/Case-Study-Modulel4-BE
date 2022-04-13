4497d79a27670d50e3b3503d2906822e8a00e848
kieuhuan <kieuhuanbka@gmail.com>
2022-04-13 09:56:21 +0700

    add env

--- src/main/java/com/codegym/case_study_m4/repository/IUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.case_study_m4.repository;
-
-import com.codegym.case_study_m4.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface
-IUserRepository extends JpaRepository<User, Long>  {
-    User findByUsername(String username);
-}
