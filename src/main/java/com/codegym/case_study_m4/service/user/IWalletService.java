00271cef566cfef77b4c1b9d95df88238d70ab21
Hungyuuki <hungyuuki777@gmail.com>
2022-04-13 11:56:30 +0700

    walletgeneralservice

---
+++ src/main/java/com/codegym/case_study_m4/service/user/IWalletService.java
@@ -0,0 +1,7 @@
+package com.codegym.case_study_m4.service.user;
+
+import com.codegym.case_study_m4.model.Wallet;
+import com.codegym.case_study_m4.service.IGeneralService;
+
+public interface IWalletService extends IGeneralService<Wallet> {
+}
