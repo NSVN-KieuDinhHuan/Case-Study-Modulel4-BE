4c03c009094eef2139544f45f08cea4c0bf9e1d8
hoale0507 <hoale090293@gmail.com>
2022-04-14 08:18:12 +0700

    payment crud

---
+++ src/main/java/com/codegym/case_study_m4/service/payment/IPaymentService.java
@@ -0,0 +1,7 @@
+package com.codegym.case_study_m4.service.payment;
+
+import com.codegym.case_study_m4.model.Payment;
+import com.codegym.case_study_m4.service.IGeneralService;
+
+public interface IPaymentService extends IGeneralService<Payment> {
+}
---
+++ src/main/java/com/codegym/case_study_m4/service/payment/PaymentService.java
@@ -0,0 +1,35 @@
+package com.codegym.case_study_m4.service.payment;
+
+import com.codegym.case_study_m4.model.Payment;
+import com.codegym.case_study_m4.repository.IPaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PaymentService implements IPaymentService{
+    @Autowired
+    private IPaymentRepository paymentRepository;
+    @Override
+    public Page<Payment> findAll(Pageable pageable) {
+        return paymentRepository.findAll(pageable);
+    }
+
+    @Override
+    public Optional<Payment> findById(Long id) {
+        return paymentRepository.findById(id);
+    }
+
+    @Override
+    public Payment save(Payment payment) {
+        return paymentRepository.save(payment);
+    }
+
+    @Override
+    public void removeById(Long id) {
+        paymentRepository.deleteById(id);
+    }
+}
