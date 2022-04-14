28fafc976ae9fa1a2a1e7b71b08c163a23ace209
Hungyuuki <hungyuuki777@gmail.com>
2022-04-14 14:28:13 +0700

    removePageableOK

--- src/main/java/com/codegym/case_study_m4/service/IGeneralService.java
+++ src/main/java/com/codegym/case_study_m4/service/IGeneralService.java
@@ -1,11 +1,12 @@
 package com.codegym.case_study_m4.service;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface IGeneralService<T> {
-    Iterable<T> findAll();
+    Page<T> findAll(Pageable pageable);
 
     Optional<T> findById(Long id);
 
--- src/main/java/com/codegym/case_study_m4/service/Wallet/WalletService.java
+++ src/main/java/com/codegym/case_study_m4/service/Wallet/WalletService.java
@@ -3,6 +3,8 @@ package com.codegym.case_study_m4.service.Wallet;
 import com.codegym.case_study_m4.model.Wallet;
 import com.codegym.case_study_m4.repository.IWalletRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,6 +17,11 @@ public class WalletService implements IWalletService {
     @Override
     public Iterable<Wallet> findAll() {
         return walletRepository.findAll();
+    }
+
+    @Override
+    public Page<Wallet> findAll(Pageable pageable) {
+        return walletRepository.findAll(pageable);
     }
 
     @Override
